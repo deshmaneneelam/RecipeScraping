4b856ca6140c5d5ebe9bd5ba9b1e169d14c49698
Neelam <deshmane.neelam@gmail.com>
2023-04-20 15:07:45 -0700

    First commit

---
+++ src/test/java/com/sdet/scraping/utilities/TestBase.java
@@ -0,0 +1,55 @@
+package com.sdet.scraping.utilities;
+
+import java.io.IOException;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestBase {
+
+	WebDriver driver;
+	
+	@BeforeClass
+	public void setup() throws IOException {
+		launchBrowser();	
+	}
+	
+	private void launchBrowser() throws IOException {
+		/*WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();*/
+		
+		
+		WebDriverManager.firefoxdriver().setup();
+		driver = new FirefoxDriver();
+		
+		ConfigReader config = new ConfigReader();	
+		String u = config.getUrl(); 
+		System.out.println("url::"+u);
+		openUrl(config.getUrl());
+	
+		
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+	}
+	
+	private void openUrl(String url) {
+		driver.get(url);
+	}
+	
+	@AfterClass
+	public void closeBrowser() {
+		driver.close();
+		driver.quit();
+	}
+	
+	@Test
+	public void testMethod() {
+		System.out.println("Here");
+	}
+}

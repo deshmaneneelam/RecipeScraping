b21a72499ada32a48908979c527bf3d1ff59c01c
jsri36 <jsri36@gmail.com>
2023-04-25 09:59:35 -0400

    Diabetes

--- src/test/java/com/sdet/scraping/utilities/Utils.java
+++ src/test/java/com/sdet/scraping/utilities/Utils.java
@@ -18,17 +18,22 @@ public class Utils {
 	public static ArrayList<String> eliminateDiabetes() {
 		String[] eliminate = {"processed grain","cream of rice","rice flour","rice rava","corn","sugar",
 				"white rice","white bread","pasta","sweetened beverage","soda","flavoured water","gatorade",
-				"fruit juice","apple juice","orange juice","pomegranate juice","flavoured curd","flavoured yogurt",
-				"breakfast cereal","corn flake","puffed rice","bran flake","instant oatmeal","honey",
-				"maple syrup","jaggery","sweet","candie","candy","chocolate","all purpose flour",
+				"fruit juice","apple juice","orange juice","pomegranate juice","margarine","peanut butter",
+				"spread","frozen food","flavoured curd","flavoured yogurt","flake","puffed rice","instant oatmeal",
+				"cereal","corn flake","puffed rice","bran flake","instant oatmeal","honey","cooked long grain",
+				"maple syrup","jaggery","sweet","candie","candy","chocolate","all purpose flour","plain flour",
 				"refined flour","multi purpose flour","alcoholic beverage","processed meat","bacon","sausage",
-				"hot dos","deli meat","chicken nugget","chciken pattie","chciken patty","jam","jelly",
-				"pickled food","mango pickle","cucumber pickle","tomatoes pickle","tomatoe pickle","chips",
-				"mayonnaise","palmolein oil","baked food","doughnut","cake","pastries","pastry","cookies",
-				"cookey","biscuit","croissant","sweetened tea","sweetened coffee","packaged snack",
-				"Packed snack","banana","melon","soft drink","dairy Milk","butter","cheese",
-				"cinnamon Toast crunch","granola",
-				"canned fruit","Canned vegetable","canned","powdered milk"
+				"hot dog","deli meat","chicken nugget","chicken pattie","chicken patty","jam","jelly","maida",
+				"pickled food","pickle","canned","chips","mayonnaise","palmolein oil","dried food","powdered milk",
+				"baked food","doughnut","cake","pastries","pastry","cookie","cookey","biscuit","croissant",
+				"sweetened tea","sweetened coffee","packaged snack","Packed snack","soft drink","banana","melon",
+				"dairy Milk","butter","cheese","cinnamon Toast crunch","granola","basmathi","basmati","rajma",
+				"cooked long grain","steamed rice","chawal","raw rice","cooked rice","chinese rice","poha",
+				"semolina","idli rawa","kabuli chana","chick pea","kabuli chana","maize flour","2 bread",
+				"pita bread","paneer","dabeli","milk","whole green gram","black eyed bean","chawli","cow pea",
+				"lobhia","soyabean","soya bean","bengal gram","garbanzo","baked bean","infused water",
+				"pineapple","peach","mango","pear","mixed fruit","mandarine orange","cherr"
+			
 				};
 		ArrayList<String> eliminateList = new ArrayList<String>(Arrays.asList(eliminate));
 		return eliminateList;
@@ -36,8 +41,9 @@ public class Utils {
 	
 	public static ArrayList<String> toAddDiabetes() {
 			String[] toadd = {"broccoli","pumpkin","pumpkin seed","chia sedd","chia seed","flaxseed",
-					"flax seed","apple","nuts","lady finger","okra","beans","raspberr","rasp berr","strawberr",
-					"straw berr","blueberr","blue berr","blackberr","black berr","egg","yogurt","bitter gaurd",
+					"flax seed","apple","nuts","lady finger","okra","beans","cluster bean","gavarfali",
+					"french bean","bean sprout","raspberr","rasp berr","strawberr","egg ","eggs",
+					"straw berr","blueberr","blue berr","blackberr","black berr","yogurt","bitter gaurd",
 					"rolled oat","steel cut oat","chicken","fish","quinoa","mushroom"
 					};
 			ArrayList<String> toaddList = new ArrayList<String>(Arrays.asList(toadd));
@@ -51,18 +57,25 @@ public class Utils {
 	}
 	
 	public static ArrayList<String> eliminateHypertension() {
-		String[] eliminate = {"salty food","snack","chips","pretzels","crackers","caffeine","coffee","tea",
-				"soft drink","alcohol","frozen food","meat","bacon","ham","pickle","processed","canned",
-				"fried food","sauce","mayonnaise","bacon","sausage","deli meat","white rice","white bread"};
+		String[] eliminate = {"salt","snack","chips","pretzels","crackers","caffeine","coffee","tea",
+				"soft drink","apple juice","alcohol","frozen food","meat","bacon","ham","ice cream","pickle",
+				"processed","canned","fried food","sauce","mayonnaise","bacon","sausage","deli meat","white rice",
+				"white bread","basmati","ginger ale",
+				"ice-cream","fruit drink","smoothie","watermelon drink","grapes juice","grape juice",
+				"pineapple juice","sharbat","orange juice","fresh Juice","pregnancy juice","sorbet","apple drink",
+				"party drink","indian drink","cranberry juice","beverage","frozen brownie","frozen muskmelon",
+				"frozen blueberr"};
 		ArrayList<String> eliminateList = new ArrayList<String>(Arrays.asList(eliminate));
 		return eliminateList;
 	}
 	
 	public static ArrayList<String> toAddHypertension() {
-		String[] toadd = {"beetroot","blueberr","strawberr","potassium rich","banana","avocado","tomato",
-				"sweet potato","mushroom","celery","kiwi","dark chocolate","watermelon","spinach","cabbage",
-				"romaine lettuce","mustard greens","broccoli","argula","fennel","kale","garlic","pomegranate",
-				"cinnamon","pistachio","chia seed","yogurt","curd","unsalted nut","chicken","fish","turkey","quinoa"};
+		String[] toadd = {"beetroot","berr","blueberr","blue berr","strawberr","straw berr","potassium rich",
+				"banana","avocado","tomato","sweet potato","mushroom","celery","kiwi","dark chocolate",
+				"watermelon","spinach","cabbage","romaine lettuce","mustard greens","broccoli","argula",
+				"fennel","kale","garlic","pomegranate","cinnamon","pistachio","chia seed","yogurt","curd",
+				"unsalted nut","chicken","fish","turkey","quinoa","badam","almond","walnut","akhrot","cashew",
+				"nuts","mixed nut"};
 		ArrayList<String> toaddList = new ArrayList<String>(Arrays.asList(toadd));
 		return toaddList;
 	}
